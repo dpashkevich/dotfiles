65e13f14c76120789640c9f1d1a3c2bcd6db399f
dpashkevich <dpashk@gmail.com>
2014-01-10 01:30:57 +0400

    Set title for GUI terminal emulators

--- cli/.bash/defs.sh
+++ cli/.bash/defs.sh
@@ -1,6 +1,14 @@
 export EDITOR=vim
 
 PS1='${debian_chroot:+($debian_chroot)}\u@\h:\W\$ '
+# If this is an xterm set the title to user@host:dir
+case "$TERM" in
+xterm*|rxvt*)
+    PS1="\[\e]0;${debian_chroot:+($debian_chroot)}\u@\h: \w\a\]$PS1"
+    ;;
+*)
+    ;;
+esac
 
 # set solarized dircolors
 export TERM=xterm-256color
