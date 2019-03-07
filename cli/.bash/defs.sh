084742e27868cc4938e587c89e58633a1a9834a5
Dmitry Pashkevich <d.pashkevich@calendly.com>
2019-03-07 10:01:16 -0500

    Update bash settings and aliases

--- cli/.bash/defs.sh
+++ cli/.bash/defs.sh
@@ -1,5 +1,7 @@
 export EDITOR=vim
 
+export HISTCONTROL=ignoreboth
+
 PS1='${debian_chroot:+($debian_chroot)}\u@\h:\W\$ '
 # If this is an xterm set the title to user@host:dir
 case "$TERM" in
@@ -12,7 +14,8 @@ esac
 
 # set solarized dircolors
 export TERM=xterm-256color
-eval `dircolors ~/dircolors-solarized`
+#eval `dircolors ~/dircolors-solarized`
+export CLICOLOR=YES
 
 # set solarized mc skin
 export MC_SKIN=~/mc-solarized.ini
